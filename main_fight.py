952ab25218dfa29e4675e0d5b7ac210d3a869dd9
NeutralVibes1 <128731687+NeutralVibes1@users.noreply.github.com>
2023-06-23 16:57:21 -0700

    Update main_fight.py
    added enum and formatted strings

--- main_fight.py
+++ main_fight.py
@@ -1,6 +1,7 @@
 import random
-import enemies, testing
+import enemies
 from player import player
+from enum import Enum
 
 # ---------------------------Seperation for visual flow------------------------------------ #
 
@@ -8,48 +9,57 @@ from player import player
 line = '-----------------\n'
 
 
-# This code chooses a random enemy from the enemies.py file and saves it to the enemy variable. The following print statements state which enemy we are fighting and their hitpoints
+# This code chooses a random enemy from the enemies.py file and saves it to the enemy variable. 
+# The following print statements state which enemy we are fighting and their hitpoints
 enemy = random.choice(enemies.enemies)
 
-print("\n" + player.name + " vs. " + enemy['name'] + "\n")
-print('{0} - {1} hitpoints'.format(player.name, player.hit_points))
-print('{0} - {1} hitpoints'.format(enemy['name'], enemy['hit_points']))
-print('\nLet the battle commence..')
+print(f"\n{player.name} vs. {enemy['name']}\n")
+print(f"{player.name} - {player.hit_points} hitpoints")
+print(f"{enemy['name']} - {enemy['hit_points']} hitpoints")
+print("\nLet the battle commence..")
 
+
+# -- Fight --
+
+class PlayerMove(Enum):
+    HEAL = 'h'
+    ATTACK = 'a'
 
 # The entire fight happens within this while loop. (While both the player and enemy are alive/ have above 0 hp)
 while (player.hit_points > 0) and (enemy['hit_points'] > 0):
     
-    # PLAYER TURN - This allows you to press the h or a key to heal or attach respectively, if you heal your hit points raise by 10, 
+    # PLAYER TURN - This allows you to press the h or a key to heal or attack respectively 
     # if you attack it rolls a hit chance based on your accuracy and if you successfully hit then it rolls the damage based on your strength
     player_move = input(line + '(Your Turn. h: Heal, a: Attack)')
     print('')
     
-    if player_move != 'h' and player_move != 'a':
-        player_move = input('(Invalid input, try again. h: Heal, a: Attack)')
-        print('')
+    while player_move not in [move.value for move in PlayerMove]:
+        player_move = input('(Invalid input, try again. h: Heal, a: Attack)\n')
 
-    if player_move == 'h':
-        player.hit_points += 10
-        print('You drink a potion\n' + 'You have ' + str(player.hit_points) + ' hitpoints remaining.\n')
-
-    elif player_move == 'a':
-        player_hit_chance = (random.choice(range(0, player.accuracy)))
+    if player_move == PlayerMove.HEAL.value:
+        if 'potion' in player.inventory:
+            player.hit_points += 10
+            print(f'You drink a potion\nYou have {player.hit_points} hit points remaining.\n')
+            player.inventory.remove('potion')
+        else:
+            print('You have no available potions')
+            continue
+    elif player_move == PlayerMove.ATTACK.value:
+        player_hit_chance = random.choice(range(0, player.accuracy))
         hit = player_hit_chance >= (player.accuracy / 2)
 
         if hit:
             hit = random.choice(range(1, player.strength))
-            print("You hit a {hit}".format(hit=hit))
-            enemy['hit_points'] = enemy['hit_points'] - hit
+            print(f'You hit a {hit}')
+            enemy['hit_points'] -= hit
 
             if enemy['hit_points'] <= 0:
-                print(enemy['name'] + " has died.\n")
+                print(f"{enemy['name']} has died.\n")
                 continue
-
-            print('(' + enemy['name'], "-", str(enemy['hit_points']) + " hit points remaining.)\n")
-        elif hit is False:
+        else:
             print("You missed!")
-            print('(' + enemy['name'], "-", str(enemy['hit_points']) + " hit points remaining.)\n")
+        
+        print(f"({enemy['name']} - {enemy['hit_points']} hit points remaining.)\n")
 
 
     # ENEMY TURN - This rolls the enemy hit chance based on their accuracy,
