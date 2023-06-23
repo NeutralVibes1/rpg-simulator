94238657c171a8606360c5808db5f179ffd87e14
NeutralVibes1 <128731687+NeutralVibes1@users.noreply.github.com>
2023-06-23 11:08:36 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,9 +1,8 @@
 # Feel free to change the name variable to your preferred player name.
 
-name = 'Tom, The Undaunted'
-
-hit_points = 150
-
-inventory = []
-
-accuracy = 8
+class player:
+    name = 'Tom, The Undaunted'
+    accuracy = 8
+    hit_points = 150
+    strength = 15
+    inventory = []
